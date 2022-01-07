3746a72e405c8ba5f5e3a9a8e056dbceac5897f8
Axel Howind <axel@dua3.com>
2022-01-07 10:43:13 +0100

    add messages

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 #FLAGS=--debug
 
 cd `dirname $0` \
-&& ./gradlew clean build \
-&& ./gradlew -Dtest test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} \
-&& ./gradlew -Dtest test-cabe-gradle-plugin-with-modules:clean test-cabe-gradle-plugin-with-modules:run ${FLAGS} \
-&& ./gradlew publishToMavenLocal \
+&& ./gradlew clean build && echo "build successful" \
+&& ./gradlew -Dtest test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} && echo "non-modular test successful" \
+&& ./gradlew -Dtest test-cabe-gradle-plugin-with-modules:clean test-cabe-gradle-plugin-with-modules:run ${FLAGS} && echo "modular test successful" \
+&& ./gradlew publishToMavenLocal && echo "plugin published to local repository" \
 && echo "SUCCESS"
