9e2f588b91509f458842d7a469ff9031d8c45af5
Axel Howind <axel@dua3.com>
2022-05-27 10:46:43 +0200

    clean after applying plugin to reveal when a file is locked after executing the gradle task

--- build.sh
+++ build.sh
@@ -4,11 +4,18 @@
 
 #FLAGS=--debug
 
+# run a clean after successful execution to check files are not locked after executing gradle task
 cd `dirname $0` \
-&& ./gradlew -Dnotest clean build publishToMavenLocal && echo "build successful" \
-&& ./gradlew test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} && echo "non-modular test successful" \
-&& ./gradlew test-cabe-gradle-plugin-with-modules:clean test-cabe-gradle-plugin-with-modules:run ${FLAGS} && echo "modular test successful" \
-&& ./gradlew publishToMavenLocal && echo "plugin published to local repository" \
+&& ./gradlew -Dnotest clean build publishToMavenLocal \
+  && echo "build successful" \
+&& ./gradlew test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} \
+  && ./gradlew test-cabe-gradle-plugin:clean \
+  && echo "non-modular test successful" \
+&& ./gradlew test-cabe-gradle-plugin-with-modules:clean test-cabe-gradle-plugin-with-modules:run ${FLAGS} \
+  && ./gradlew  test-cabe-gradle-plugin-with-modules:clean \
+  && echo "modular test successful" \
+&& ./gradlew publishToMavenLocal \
+  && echo "plugin published to local repository" \
 || { echo "ERROR" ; exit 1 ; }
 
 read -p "Publish library and plugin? " -n 1 -r
@@ -16,6 +23,6 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   ./gradlew cabe-gradle-plugin:publishPlugins publish && echo "plugin published"
-fi 
+fi
 
 echo "SUCCESS"
