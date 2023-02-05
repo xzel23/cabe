d997ca77c57c05d2a02adfd00a282aad40425fd2
Axel Howind <axh@dua3.com>
2023-02-05 14:45:18 +0100

    plugin version 1.2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 #FLAGS=--debug
 
 # run a clean after successful execution to check files are not locked after executing gradle task
-cd `dirname $0` \
+cd "`dirname $0`" \
 && ./gradlew -Dnotest clean build publishToMavenLocal \
   && echo "build successful" \
 && ./gradlew test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} \
@@ -18,11 +18,13 @@ cd `dirname $0` \
   && echo "plugin published to local repository" \
 || { echo "ERROR" ; exit 1 ; }
 
-read -p "Publish library and plugin? " -n 1 -r
+read -p "Publish plugin? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  ./gradlew cabe-gradle-plugin:publishPlugins publish && echo "plugin published"
+  ./gradlew cabe-gradle-plugin:publishPlugins && echo "plugin published"
 fi
+
+echo "to publish the library, run ./gradlew publish"
 
 echo "SUCCESS"
