ebe845a1e381b0f7088ae487812509e647b7dba4
Axel Howind <axh@dua3.com>
2023-12-29 00:48:56 +0100

    run a second build of the test to provoke error because of existing output folders

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 # This script first builds the plugin and then runs a test to verify assertions are present in the generated output 
 
-#FLAGS=--debug
+FLAGS=--stacktrace
 
-# run a clean after successful execution to check files are not locked after executing gradle task
+# run a second build and then a clean after successful execution to check files are not locked after executing gradle task
 cd "`dirname $0`" \
 && ./gradlew -Dnotest clean build test publishToMavenLocal \
   && echo "build successful" \
 && ./gradlew --no-daemon test-cabe-gradle-plugin:clean test-cabe-gradle-plugin:run ${FLAGS} \
+&& ./gradlew --no-daemon test-cabe-gradle-plugin:build ${FLAGS} \
 && ./gradlew --no-daemon test-cabe-gradle-plugin:clean ${FLAGS} \
   && echo "non-modular test successful" \
 && ./gradlew --no-daemon test-cabe-gradle-plugin-with-modules:clean test-cabe-gradle-plugin-with-modules:run ${FLAGS} \
