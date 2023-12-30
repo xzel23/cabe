8e9159b3830c72dc4d0aa64f04b0fb0875f662ac
Axel Howind <axh@dua3.com>
2023-12-30 15:31:41 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("java")
 }
 
-extra["plugin_version"] = "2.0-rc12"
-extra["processor_version"] = "2.0.5"
+extra["plugin_version"] = "2.0-rc13"
+extra["processor_version"] = "2.0.6"
 extra["annotations_version"] = "2.0"
 
 subprojects {
