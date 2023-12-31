1fb0114741d93c0b9c5a4384c2d4d497d65e9c93
Axel Howind <axh@dua3.com>
2023-12-31 11:12:20 +0100

    v2.0-rc14

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("java")
 }
 
-extra["plugin_version"] = "2.0-rc13"
-extra["processor_version"] = "2.0.6"
+extra["plugin_version"] = "2.0-rc14"
+extra["processor_version"] = "2.0.7"
 extra["annotations_version"] = "2.0"
 
 subprojects {
