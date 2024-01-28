4b4c66712e6539d2c187bae60a6db0ab14b31f4c
Axel Howind <axh@dua3.com>
2024-01-28 10:54:45 +0100

    explicitly set config

--- cabe-gradle-plugin-test/test-gradle-plugin/build.gradle.kts
+++ cabe-gradle-plugin-test/test-gradle-plugin/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.dua3.cabe.gradle.CabeExtension
+import com.dua3.cabe.processor.Config
+
 buildscript {
     repositories {
         mavenLocal()
@@ -14,6 +17,10 @@ plugins {
 }
 
 apply(plugin = "com.dua3.cabe")
+
+configure<CabeExtension> {
+    config.set(Config.StandardConfig.STANDARD.config)
+}
 
 repositories {
     mavenLocal()
