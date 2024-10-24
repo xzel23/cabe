b34c5b12a3a75d04f9438079be18f0287ad67ec5
Axel Howind <axh@dua3.com>
2024-10-24 18:19:24 +0200

    v3.0-beta-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("com.dorongold.task-tree") version "2.1.1"
 }
 
-extra["plugin_version"] = "3.0-beta-1"
-extra["processor_version"] = "3.0-beta-1"
+extra["plugin_version"] = "3.0-beta-2"
+extra["processor_version"] = "3.0-beta-2"
 
 subprojects {
     apply(plugin = "java")
