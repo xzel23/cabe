919714325f72bdafbfc2cb1b521bf21c03a0f644
Axel Howind <axh@dua3.com>
2024-11-22 12:12:18 +0100

    release version 3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("com.dorongold.task-tree") version "2.1.1"
 }
 
-extra["plugin_version"] = "3.0-rc"
-extra["processor_version"] = "3.0-rc"
+extra["plugin_version"] = "3.0.0"
+extra["processor_version"] = "3.0.0"
 
 subprojects {
     apply(plugin = "java")
