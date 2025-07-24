2377bb8b0705e716d4b9756cb6c1af62adf55e16
Axel Howind <axh@dua3.com>
2025-07-24 03:25:13 +0200

    fix toolchain setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,6 +33,20 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
+
 rootProject.name = "cabe"
 
 if (System.getProperty("test") == null) {
