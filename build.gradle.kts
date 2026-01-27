3736fd138374b544fb9cd452ab82b2a8785d3a09
Axel Howind <axh@dua3.com>
2026-01-27 04:02:45 +0100

    bump version to 4.1 for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ extra["ORGANIZATION_NAME"]  = "dua3"
 extra["ORGANIZATION_URL"]   = "https://www.dua3.com"
 /////////////////////////////////////////////////////////////////////////////
 
-val projectVersion = "4.1-SNAPSHOT"
+val projectVersion = "4.1"
 version = projectVersion
 extra["plugin_version"] = projectVersion
 extra["processor_version"] = projectVersion
