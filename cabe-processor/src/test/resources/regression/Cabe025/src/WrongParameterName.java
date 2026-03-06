141651a1ee5eab976075796b978da484fe568b2c
Axel Howind <axh@dua3.com>
2026-03-06 14:47:46 +0100

    remove test case Cabe025 and redundant Maven setup in CI configuration

--- cabe-processor/src/test/resources/regression/Cabe025/src/WrongParameterName.java
+++
@@ -1,8 +0,0 @@
-import org.jspecify.annotations.*;
-import java.util.function.Supplier;
-
-@NullMarked
-public class WrongParameterName {
-    public void filterAndDispatch(long timestamp, String loggerName, Object lvl, @Nullable String mrk, @Nullable Object mdc, Object locationResolver, Supplier<CharSequence> msg, @Nullable Throwable t) {
-    }
-}
