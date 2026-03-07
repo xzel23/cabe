392f84fe6793340cd893c09cd1cbbb3a9d4979ac
Axel Howind <axh@dua3.com>
2026-03-07 08:45:45 +0100

    remove obsolete build.sh script after migration to Gradle build logic

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is now a wrapper around the Gradle fullBuild task.
-# The complete build logic has been moved to build.gradle.kts.
-
-cd "$(dirname "$0")" || exit 1
-./gradlew fullBuild "$@"
