81a499dc4d74feab560a46071b3df29a0e0e5fdf
Axel Howind <axh@dua3.com>
2026-03-07 12:27:10 +0100

    bump version to 4.2.0-rc3

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ extra["ORGANIZATION_NAME"]  = "dua3"
 extra["ORGANIZATION_URL"]   = "https://www.dua3.com"
 /////////////////////////////////////////////////////////////////////////////
 
-val projectVersion = "4.2.0-rc3-SNAPSHOT"
+val projectVersion = "4.2.0-rc3"
 version = projectVersion
 extra["plugin_version"] = projectVersion
 extra["processor_version"] = projectVersion
